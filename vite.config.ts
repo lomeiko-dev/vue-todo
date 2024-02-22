13b384d99ed530f4120788259bf69280cb04c4f6
Nikita Lomeiko <lomieiko78@mail.ru>
2024-02-22 16:38:22 +0300

    feat: init structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      app: "/src/app",
+      pages: "/src/pages",
+      widgets: "/src/widgets",
+      features: "/src/features",
+      entities: "/src/entities",
+      shared: "/src/shared",
+      server: "/server",
+    },
+  },
+});
